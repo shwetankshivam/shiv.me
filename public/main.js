9915f7ccd14f60e0ed5ac7f3e54b92b6e14fc2fa
shiv <shwetankshivam@gmail.com>
2024-04-27 14:10:47 +0530

    enhanced Welcome Screen, updated CV

--- public/main.js
+++ public/main.js
@@ -1,54 +1,70 @@
 let intro = document.querySelector('.splash');
 let logo = document.querySelector('.logo-header');
-let logoSpan = document.querySelectorAll('.logo');
+let logoSpan = document.querySelectorAll('#logo');
+
 let time = document.querySelector('#time');
 let date = document.querySelector('#date');
 
+document.addEventListener("DOMContentLoaded", function () {
+
+    window.addEventListener('DOMContentLoaded', () => {
 
 
-window.addEventListener('DOMContentLoaded', () => {
+        const greetings = [ "Hello", "नमस्ते", "Bonjour", "Hola", "Ciao", "Hallo", "Привет", "こんにちは", "안녕하세요", "مرحبا" , "Nǐ hǎo"];
 
-    setTimeout(() => {
+        // Select a random greeting from the array
+        const randomIndex = Math.floor(Math.random() * greetings.length);
+        const randomGreeting = greetings[randomIndex];
 
-        logoSpan.forEach((span, idx) => {
-            setTimeout(() => {
-                span.classList.add('active');
-            }, (idx + 1) * 150)
-        });
+        // Display the random greeting on the splash screen
+        const greetingElement = document.getElementById("logo");
+        greetingElement.textContent = randomGreeting;
+
+
 
         setTimeout(() => {
+
             logoSpan.forEach((span, idx) => {
+                setTimeout(() => {
+                    span.classList.add('active');
+                }, (idx + 1) * 150)
+            });
 
-                span.classList.remove('active');
-                span.classList.add('fade');
-            }, (idx + 1) * 50)
-        }, 20);
+            setTimeout(() => {
+                logoSpan.forEach((span, idx) => {
+
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 50)
+            }, 20);
 
 
-        setTimeout(() => {
-            intro.style.top = '-100vh';
-            intro.style.opacity = '0';
-        }, 1000)
+            setTimeout(() => {
+                intro.style.top = '-100vh';
 
-    })
+                intro.style.opacity = '0';
+
+            }, 1000)
+
+        })
+
+
+
+    });
+
+    setInterval(() => {
+        let t = new Date();
+        time.innerHTML = t.toLocaleTimeString();
+
+
+    }, 1000);
+
+    setInterval(() => {
+        let d = new Date();
+        date.innerHTML = d.toLocaleDateString();
+
+    }, 1000);
 
 
 
 });
-
-setInterval(() => {
-    let t = new Date();
-    time.innerHTML = t.toLocaleTimeString();
-
-
-}, 1000);
-
-setInterval(() => {
-    let d = new Date();
-    date.innerHTML = d.toLocaleDateString();
-
-}, 1000);
-
-
-
-
